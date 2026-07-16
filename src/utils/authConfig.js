bb1cf255f0bf4b544018af57af62e4d718c33d00
Ajay <ajaycharve109@gmail.com>
2026-07-16 17:39:26 +0530

    udpate level image bug

--- src/utils/authConfig.js
+++ src/utils/authConfig.js
@@ -1,12 +1,63 @@
+// export const getAuthConfig = () => {
+//     const token = localStorage.getItem("token");
+
+//     return {
+//         headers: {
+//             Authorization: `Bearer ${token}`,
+//             "Accept-Language": "en",
+//             "Content-Type": "application/json",
+//             Accept: "application/json",
+//         },
+//     };
+// };
+
+// utils/authConfig.js
+let isFileUpload = false;
+
+// Function to set file upload mode (called from interceptor)
+export const setFileUploadMode = (mode) => {
+    isFileUpload = mode;
+};
+
 export const getAuthConfig = () => {
     const token = localStorage.getItem("token");
 
-    return {
-        headers: {
-            Authorization: `Bearer ${token}`,
-            "Accept-Language": "en",
-            "Content-Type": "application/json",
-            Accept: "application/json",
-        },
+    const headers = {
+        Authorization: `Bearer ${token}`,
+        "Accept-Language": "en",
+        Accept: "application/json",
     };
+
+    // Only add Content-Type for non-file uploads
+    if (!isFileUpload) {
+        headers["Content-Type"] = "application/json";
+    }
+
+    // Reset the flag after use
+    isFileUpload = false;
+
+    return { headers };
 };
+
+// Add axios interceptor to detect FormData automatically
+import axiosInstance from "../app/axios";
+
+// Request interceptor to detect FormData
+axiosInstance.interceptors.request.use(
+    (config) => {
+        // Check if the request data is FormData
+        if (config.data instanceof FormData) {
+            setFileUploadMode(true);
+            // Remove Content-Type header if present to let browser set it
+            if (config.headers['Content-Type']) {
+                delete config.headers['Content-Type'];
+            }
+        } else {
+            setFileUploadMode(false);
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
