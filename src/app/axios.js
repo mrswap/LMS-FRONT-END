66ff5629518a2a89a3b0a988239e923c5f068031
Ajay <ajaycharve109@gmail.com>
2026-08-31 18:03:29 +0530

    update

--- src/app/axios.js
+++ src/app/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    // baseURL: "https://lms-backend.netswaptech.com/api/v1/admin",
-    baseURL: "https://backend.avantemedical.co.in/api/v1/admin",
+    baseURL: "https://lms-backend.netswaptech.com/api/v1/admin",
+    // baseURL: "https://backend.avantemedical.co.in/api/v1/admin",
     withCredentials: false,
 });
 
