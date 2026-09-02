ac9093815d2801436b3086317f5a92752a8b02c6
Ajay <ajaycharve109@gmail.com>
2026-09-02 11:42:23 +0530

    update

--- src/app/axiosCommon.js
+++ src/app/axiosCommon.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosCommonInstance = axios.create({
-    // baseURL: "https://lms-backend.netswaptech.com/api/v1",
-    baseURL: "https://backend.avantemedical.co.in/api/v1",
+    baseURL: "https://lms-backend.netswaptech.com/api/v1",
+    // baseURL: "https://backend.avantemedical.co.in/api/v1",
     // withCredentials: false,
     withCredentials: false,
 
